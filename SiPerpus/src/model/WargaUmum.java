368fc0aa7541780fb9f5ee75e73d7c1c2dac3403
puspaningtyas <puspaningtyas@8b12e590-4eed-fbe2-8d2c-e36e88e00786>
2011-08-23 11:36:53 +0000

    buat kelas untuk entity sistem informasi Perpustakaan

---
+++ SiPerpus/src/model/WargaUmum.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author puspa
+ */
+public class WargaUmum extends Pengguna{
+
+}
