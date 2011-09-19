7913a76e3ed1e609ddb9f10c3f905f35446291f8
puspaningtyas <puspaningtyas@8b12e590-4eed-fbe2-8d2c-e36e88e00786>
2011-09-19 09:04:16 +0000

    tambah penulis di kelas buku

--- SiPerpus/test/test/model/TestPengguna2.java
+++ SiPerpus/test/test/model/TestPengguna2.java
@@ -20,25 +20,35 @@ public class TestPengguna2 {
 
         Buku testBuku = new Buku();
         testBuku.setJudul("Laskar Pelangi");
-        System.out.println("judul = "+testBuku.getJudul());
+        System.out.println("judul = " + testBuku.getJudul());
 
 
         Buku testBuku1 = new Buku();
         testBuku1.setJudul("Sang Pemimpi");
-        System.out.println("judul = "+testBuku1.getJudul());
+        System.out.println("judul = " + testBuku1.getJudul());
 
         Date now = Calendar.getInstance().getTime();
         Date yesterday = new Date(111, 7, 3);
 
+        //pinjam buku
         testPengguna.tambahDaftarPinjaman(testBuku, yesterday);
         testPengguna.tambahDaftarPinjaman(testBuku1, yesterday);
 
-        System.out.println("Pengguna = "+testPengguna.getNama());
+        System.out.println("Pengguna = " + testPengguna.getNama());
         System.out.println("Daftar Pinjaman");
         System.out.println(testPengguna.tampilPinjaman());
 
-        System.out.println("terlambat = "+testPengguna.adaDaftarPinjamanTerlambat(now));
-        System.out.println("denda  = "+testPengguna.hitungDenda(now));
+        System.out.println("terlambat = " + testPengguna.adaDaftarPinjamanTerlambat(now));
+        System.out.println("denda  = " + testPengguna.hitungDenda(now));
+
+        // kembalikan buku
+        testPengguna.kembalikanPinjaman(testBuku);
+
+        System.out.println("Ada "+testBuku.getJudul()+" di daftar Pinjam? " +testPengguna.adaKoleksi(testBuku));
+        System.out.println("Pengguna = " + testPengguna.getNama());
+        System.out.println("Daftar Pinjaman");
+        System.out.println(testPengguna.tampilPinjaman());
+
 
     }
 }
